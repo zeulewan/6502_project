39a0f0c427d81d92873df18f16356803009005d7
Zeulewan Mordasiewicz <zeul@mordasiewicz.com>
2024-07-23 21:33:44 -0400

    🔧 (makerom.py): add initial ROM values and setup reset vector for EEPROM chip enable

--- makerom.py
+++ makerom.py
@@ -4,6 +4,10 @@ rom = bytearray([0xea] * 32768)
 rom[0] = 0xa9
 rom[1] = 0x42
 
+rom[2] = 0x8d
+rom[3] = 
+
+
 #reset vector set up, send to address '1000000000000000' leading zero is for EEPROM chip enable
 rom[0x7ffc] = 0x00 
 rom[0x7ffd] = 0x80
