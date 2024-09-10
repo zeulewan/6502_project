f6f25bbf7646129add187ef7076025d0a5157082
Zeulewan Mordasiewicz <zeul@mordasiewicz.com>
2024-09-10 18:22:53 -0400

    ♻️ (makerom.py): refactor code to use bytearray and remove redundant comments ♻️ (makerom.py): simplify initialization of rom array by calculating its length based on existing code

--- makerom.py
+++ makerom.py
@@ -1,17 +1,14 @@
-# initialize bite array with 0xea which is the 'no operation' function
-rom = bytearray([0xea] * 32768)
+code = bytearray([
+    0xa9, 0x42 # load accumulator with memory, memory
+    0x8d, 0x00, 0x60 # store accumulator in memory, memory, memory
+])
 
-# start program
-rom[0] = 0xa9 # load accumulator with memory
-rom[1] = 0x42 # memory
+# init bite array with 0xea which is the 'no operation' function
+rom = bytearray([0xea] * (32768 - len(code)))
 
-rom[2] = 0x8d # store accumulator in memory
-rom[3] = 0x00 # memory
-rom[4] = 0x60 # memory
 
-# reset vector set up (reset vector is first memory location gone to by default, 
+## reset vector set up (reset vector is first memory location gone to by default, 
 # the program goes to whatever is in the reset vector then counts up)
-
 # Send to address '1000000000000000' leading zero is for EEPROM chip enable
 rom[0x7ffc] = 0x00 # points to address 8000
 rom[0x7ffd] = 0x80
