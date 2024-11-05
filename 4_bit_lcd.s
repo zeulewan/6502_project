0ad9f1546cc0347483f9eaab5483e1c292ccbff8
Zeulewan Mordasiewicz <zeul@mordasiewicz.com>
2024-11-04 19:17:35 -0500

    refactor comments

--- 4_bit_lcd.s
+++ 4_bit_lcd.s
@@ -1,52 +1,52 @@
 ; Define port addresses
-PORTB = 0x6000 ; Port B
-PORTA = 0x6001 ; Port A
-DDRB = 0x6002  ; Data Direction Register B
-DDRA = 0x6003  ; Data Direction Register A
-PCR = 0x600c   ; Peripheral Control Register
-IFR = 0x600d   ; Interrupt Flag Register
-IER = 0x600e   ; Interrupt Enable Register
+PORTB = 0x6000  ; Port B address
+PORTA = 0x6001  ; Port A address
+DDRB  = 0x6002  ; Data Direction Register for Port B
+DDRA  = 0x6003  ; Data Direction Register for Port A
+PCR   = 0x600c  ; Peripheral Control Register
+IFR   = 0x600d  ; Interrupt Flag Register
+IER   = 0x600e  ; Interrupt Enable Register
 
-E = 0b10000000  ; Enable
-RW = 0b01000000 ; Read/Write (0 = write)
-RS = 0b00100000 ; Register Select
+E  = 0b10000000   ; Enable bit for LCD
+RW = 0b01000000   ; Read/Write (0 = write)
+RS = 0b00100000   ; Register Select for LCD
 
-value = 0x0200   ; 2 bytes
-mod10 = 0x0202   ; 2 bytes
-message = 0x0204 ; 6 bytes
-counter = 0x020a ; 2 bytes
-
+value    = 0x0200  ; 2 bytes to store current value
+mod10    = 0x0202  ; 2 bytes to store modulus result
+message  = 0x0204  ; 6 bytes to store output message
+counter  = 0x020a  ; 2 bytes for counter storage
 
 ; Reset routine
-    .org 0x8000 ; Set relative start location
+.org 0x8000      ; Set reset vector starting location
+
 reset:
     ; Initialize stack pointer
     ldx #0xff
     txs
-    cli ; Clear interrupt disable bit
+    cli           ; Clear interrupt disable flag
 
     lda #01
-    sta PCR
+    sta PCR       ; Enable Peripheral Control Register
 
     lda #0x82
-    sta IER ; Set interrupt enable
+    sta IER       ; Set interrupt enable
 
-    ; Set Port B pins to output
+    ; Configure Port B pins as output
     lda #0b11111111
     sta DDRB
 
-    ; Set Port A pins to input
+    ; Configure Port A pins as input
     lda #0b00000000
     sta DDRA
 
     jsr lcd_init
 
-    ; Configure LCD display
+    ; LCD Display configuration
     lda #0b00101000 ; 4-bit mode, 2 lines, 5x8 font
     jsr lcd_instruction
-    lda #0b00001111 ; Display on
+    lda #0b00001111 ; Display ON, cursor OFF
     jsr lcd_instruction
-    lda #0b00000110 ; Entry mode
+    lda #0b00000110 ; Set entry mode
     jsr lcd_instruction
     lda #0b00000001 ; Clear display
     jsr lcd_instruction
@@ -56,84 +56,83 @@ reset:
     sta counter + 1
 
 loop:
-    lda #0b00000010 ; Home cursor
+    lda #0b00000010 ; Move cursor to home position
     jsr lcd_instruction
 
     lda #0
-    sta message
+    sta message     ; Clear message buffer
 
-; Begin number conversion
+; Start decimal conversion process
     sei
-    lda counter     ; Load lower byte
+    lda counter     ; Load lower byte of counter
     sta value
-    lda counter + 1 ; Load upper byte
+    lda counter + 1 ; Load upper byte of counter
     sta value + 1
     cli
 
 divide:
-    ; Initialize remainder
+    ; Initialize remainder to zero
     lda #0
     sta mod10
     sta mod10 + 1
     clc
 
-    ldx #16 ; load x register with ascii 16
+    ldx #16         ; Set X register to process 16 bits
+
 divloop:
-    ; Rotate quotient and remainder
+    ; Rotate quotient and remainder bits
     rol value
     rol value + 1
     rol mod10
     rol mod10 + 1
 
-    ; a,y = dividend - divisor:
-    sec ; set carry 1
+    ; Subtract divisor (10) from dividend
+    sec             ; Set carry for subtraction
     lda mod10
-    sbc #10 ; subtract 10
-    tay ; save low byte in y
+    sbc #10         ; Subtract 10 from mod10
+    tay             ; Save low byte of remainder in Y
     lda mod10 + 1
-    sbc #0 ; subtract 0. since it's a subtract with carry, a reg ends up in 255 if carry is 0 from previous subtraction. SBC inverts C and subtracts it
-    bcc ignore_result ; branch if dividend < divisor. branch if carry = 0
-    sty mod10
-    sta mod10 + 1
-    
+    sbc #0          ; Subtract zero from high byte with carry
+    bcc ignore_result ; Branch if result is negative
+
+    sty mod10       ; Store remainder in mod10
+    sta mod10 + 1   ; Store high byte
+
 ignore_result:
-    dex ; decrement x register
-    bne divloop ; branch if zero flag is not set, aka brach not equal. a reg is not zero question: why isn't it just a bcc divloop?
-    rol value
+    dex             ; Decrement X register
+    bne divloop     ; Continue if X is not zero
+    rol value       ; Rotate final bits in value
     rol value + 1
 
     lda mod10
-    clc ; clear carry
-    adc #"0" ; adding 0 converts it to an ascii number
+    clc
+    adc #"0"        ; Convert to ASCII
     jsr push_char
 
     lda value 
     ora value + 1 
-    bne divide ; this says if there is still shit to divide, keep looping
+    bne divide      ; Continue division if value is non-zero
 
     ldx #0
 print_message:
     lda message,x
-    beq loop
+    beq loop        ; Loop if end of message
     jsr print_char
     inx
     jmp print_message
 
-
-; number: .word 1729 ; 1729 is the number to be converted to decimal,
-; counter used to be replaced with number
-
+; Push character to message buffer
 push_char:
     pha
     ldy #0
 
-char_loop
-    lda message,y   ; load a reg with message bit
-    tax             ; transfer a reg to x reg
-    pla             ; pull stack to a reg
-    sta message,y   ; store a reg in message bit
+char_loop:
+    lda message,y   ; Load current byte in message buffer
+    tax
+    pla
+    sta message,y   ; Store accumulator in message buffer
     iny
-    txa             ; where is null terminator coming from
+    txa
     pha
     bne char_loop
 
@@ -142,14 +141,14 @@ char_loop
     rts
 
 lcd_wait:
-    pha ; push accumulator to stack
-    lda #0b11110000; 1 output, 0 input. outputting to control pins
+    pha
+    lda #0b11110000 ; Set Port B to output mode for control signals
     sta DDRB 
 
 lcd_busy:
-    lda #RW ; tells LCD to output
+    lda #RW         ; Set RW for read mode
     sta PORTB 
-    lda #(RW | E)
+    lda #(RW | E)   ; Set RW and Enable bits
     sta PORTB
     lda PORTB
     pha
@@ -159,39 +158,39 @@ lcd_busy:
     sta PORTB
     lda PORTB
     pla
-    and #0b00001000
-    bne lcd_busy ; if zero processor flag is set to 1, loop to lcd_wait
+    and #0b00001000 ; Check busy flag
+    bne lcd_busy    ; Wait if busy flag is set
 
     lda #RW
-    sta PORTB   ; clear RS/RW/E bits
-    lda #0b11111111 ; ff Sets all pins on port B to output
-    sta DDRB ; 6002 is register for data direction for register B
-    pla ; pull to accumulator from stack
+    sta PORTB       ; Clear control bits
+    lda #0b11111111 ; Set Port B to output mode
+    sta DDRB
+    pla
     rts
 
 lcd_init:
-  lda #%00000011 ; 1st Set 8-bit mode
+  lda #%00000011   ; Set 8-bit mode (first initialization)
   sta PORTB
   ora #E
   sta PORTB
   and #%00001111
   sta PORTB
 
-  lda #%00000011 ; 2nd Set 8-bit mode
+  lda #%00000011   ; Set 8-bit mode (second initialization)
   sta PORTB
   ora #E
   sta PORTB
   and #%00001111
   sta PORTB
 
-  lda #%00000011 ; 3rd Set 8-bit mode
+  lda #%00000011   ; Set 8-bit mode (third initialization)
   sta PORTB
   ora #E
   sta PORTB
   and #%00001111
   sta PORTB
 
-  lda #%00000010 ; Set 4-bit mode
+  lda #%00000010   ; Set 4-bit mode
   sta PORTB
   ora #E
   sta PORTB
@@ -199,28 +198,27 @@ lcd_init:
   sta PORTB
   rts
 
-lcd_instruction: ; changed for 4 bit LCD operation 
+lcd_instruction:
     jsr lcd_wait
     pha
-    lsr 
     lsr
-    lsr ; logical shift right. not rotate right. fills left bit with zero 0010 1000 -> 0000 0010
+    lsr
+    lsr
     lsr
     sta PORTB
-    ora #E ; 10000000 or 1000 0010 -> 1000 0010
+    ora #E
     sta PORTB
-    eor #E         ; Clear E bit
+    eor #E
     sta PORTB
     pla
-    and #%00001111 ; Send low 4 bits
+    and #%00001111
     sta PORTB
-    ora #E         ; Set E bit to send instruction
+    ora #E
     sta PORTB
-    eor #E         ; Clear E bit
+    eor #E
     sta PORTB
     rts
 
-; Print character on LCD
 print_char:
     jsr lcd_wait
     pha 
@@ -236,12 +234,12 @@ print_char:
     sta PORTB
 
     pla 
-    and #%00001111 
+    and #%00001111
     ora #RS
     sta PORTB
     ora #E
     sta PORTB 
-    eor #E          ; possible to just send all zeros
+    eor #E
     sta PORTB
     rts
  
@@ -254,8 +252,8 @@ irq:
 
 nmi:
     rti
-    
-    .org 0xfffa
-    .word nmi
-    .word reset
-    .word irq
+
+.org 0xfffa
+.word nmi
+.word reset
+.word irq
